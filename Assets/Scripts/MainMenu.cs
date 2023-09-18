0069f2559d2cd34b6e6e444ca73a63f45b346231
Heidge <axel.gore35@gmail.com>
2023-09-18 19:38:00 +0200

    readme completed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+
+	/// <summary>
+	/// public Material trapMat = new Color32(255, 112, 0, 1);
+	/// </summary>
+	///public Material goalMat = Color.blue;
+	public Toggle colorblindMode;
 
 	public void PlayMaze()
 	{
